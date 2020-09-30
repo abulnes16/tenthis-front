5ee66bd3da267ea5c99acf25f791f74e0ed3a958
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-09-30 17:36:27 -0600

    Maquetado registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   }
 ];
 
