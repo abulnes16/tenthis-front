c687d159721f9a9c929d97133b637bef0afb6442
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-10-05 10:25:37 -0600

    Agregado componentes para página de clientes

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientLayoutComponent } from './components/client-layout/client-layout.component';
 import { SharedModule } from '../shared/shared.module';
+import { StoreListComponent } from './components/store-list/store-list.component';
+import { ClientOrdersComponent } from './components/client-orders/client-orders.component';
 
 
 @NgModule({
-  declarations: [ClientLayoutComponent],
+  declarations: [ClientLayoutComponent, StoreListComponent, ClientOrdersComponent],
   imports: [
     CommonModule,
     ClientRoutingModule,
