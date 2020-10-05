7128f40056b55ff6b564bd6419206bae22257afc
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-10-04 21:39:17 -0600

    Maquetado página de tiendas

--- src/app/admin/components/admin-stores/admin-stores.component.ts
+++ src/app/admin/components/admin-stores/admin-stores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBan, faCampground, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-admin-stores',
@@ -6,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-stores.component.scss']
 })
 export class AdminStoresComponent implements OnInit {
+
+
+  tentIcon = faCampground;
+  blockIcon = faBan;
+  deleteIcon = faTrash;
+
+  stores = [
+    {
+      name: 'Tienda',
+    },
+    {
+      name: 'Tienda',
+    },
+    {
+      name: 'Tienda',
+    },
+    {
+      name: 'Tienda',
+    },
+    {
+      name: 'Tienda',
+    },
+    {
+      name: 'Tienda',
+    },
+  ];
 
   constructor() { }
 
