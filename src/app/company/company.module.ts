7f4373014f2a9590f96824129ff0d9c0e28d0c02
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-10-05 10:23:30 -0600

    Agregado componentes para paginas de una compañia

--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -4,10 +4,17 @@ import { CommonModule } from '@angular/common';
 import { CompanyRoutingModule } from './company-routing.module';
 import { CompanyLayoutComponent } from './components/company-layout/company-layout.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductsComponent } from './components/products/products.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { CompanyPagesComponent } from './components/company-pages/company-pages.component';
+import { MediaComponent } from './components/media/media.component';
+import { ConfigComponent } from './components/config/config.component';
+import { ManagePageComponent } from './components/manage-page/manage-page.component';
 
 
 @NgModule({
-  declarations: [CompanyLayoutComponent],
+  declarations: [CompanyLayoutComponent, ProductsComponent, CategoriesComponent, OrdersComponent, CompanyPagesComponent, MediaComponent, ConfigComponent, ManagePageComponent],
   imports: [
     CommonModule,
     CompanyRoutingModule,
