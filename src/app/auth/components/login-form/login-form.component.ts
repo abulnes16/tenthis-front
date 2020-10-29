c19a2cd2c5564de4dafcecfe4c37e0f4a2565465
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-10-28 23:25:41 -0600

    Agregado funcionalidad de formulario de login

--- src/app/auth/components/login-form/login-form.component.ts
+++ src/app/auth/components/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { emailRegex } from 'src/app/constants/regex';
 
 @Component({
   selector: 'app-login-form',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
   constructor() { }
+
+  get email(): AbstractControl {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl {
+    return this.loginForm.get('password');
+  }
 
   ngOnInit(): void {
   }
