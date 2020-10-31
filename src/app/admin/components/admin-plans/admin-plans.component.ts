f685d6e77aa3d6845e3ea7cf3a60e02a5584551e
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-10-30 21:13:11 -0600

    Agregado validaciones en formulario de planes

--- src/app/admin/components/admin-plans/admin-plans.component.ts
+++ src/app/admin/components/admin-plans/admin-plans.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faTag } from '@fortawesome/free-solid-svg-icons';
+import Plan from 'src/app/models/plan';
 
 @Component({
   selector: 'app-admin-plans',
@@ -8,28 +9,54 @@ import { faTag } from '@fortawesome/free-solid-svg-icons';
 })
 export class AdminPlansComponent implements OnInit {
 
+  editMode = false;
   planIcon = faTag;
-  plans = [
+  plans: Plan[] = [
     {
-      name: 'Plan'
+      name: 'Plan',
+      price: 0,
+      storage: 0,
+      numPages: 0,
+      numProducts: 0,
+      numTemplates: 0,
     },
     {
-      name: 'Plan'
+      name: 'Plan',
+      price: 0,
+      storage: 0,
+      numPages: 0,
+      numProducts: 0,
+      numTemplates: 0,
     },
     {
-      name: 'Plan'
+      name: 'Plan',
+      price: 0,
+      storage: 0,
+      numPages: 0,
+      numProducts: 0,
+      numTemplates: 0,
     },
     {
-      name: 'Plan'
+      name: 'Plan',
+      price: 0,
+      storage: 0,
+      numPages: 0,
+      numProducts: 0,
+      numTemplates: 0,
     },
-    {
-      name: 'Plan'
-    }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  newPlan(): void {
+    this.editMode = false;
+  }
+
+  editPlan(): void {
+    this.editMode = true;
   }
 
 }
