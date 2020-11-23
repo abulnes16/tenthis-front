3cd837256c3d59232841ccd95efd4443dc7edeaa
Angel Bulnes <angel_bulnes16@yahoo.com>
2020-11-23 13:09:05 -0600

    Añadido funcionalidad de agregar multimedia

--- src/app/company/components/media/media.component.ts
+++ src/app/company/components/media/media.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { faFile } from '@fortawesome/free-solid-svg-icons';
-
+import { MediaService } from '../../../core/services/company/media.service';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import APIResponse from 'src/app/models/response';
+import Media from 'src/app/models/media';
+import Swal from 'sweetalert2';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
 @Component({
   selector: 'app-media',
   templateUrl: './media.component.html',
@@ -9,32 +14,13 @@ import { faFile } from '@fortawesome/free-solid-svg-icons';
 export class MediaComponent implements OnInit {
 
   fileIcon = faFile;
-  images = [
-    {
-      name: 'Nombre imagen',
-      url: './assets/images/shopping.jpg'
-    },
-    {
-      name: 'Nombre imagen',
-      url: './assets/images/shopping.jpg'
-    },
-    {
-      name: 'Nombre imagen',
-      url: './assets/images/shopping.jpg'
-    },
-    {
-      name: 'Nombre imagen',
-      url: './assets/images/shopping.jpg'
-    },
-    {
-      name: 'Nombre imagen',
-      url: './assets/images/shopping.jpg'
-    },
-    {
-      name: 'Nombre imagen',
-      url: './assets/images/shopping.jpg'
-    },
-  ];
+  file: any;
+  fileName: string;
+  @ViewChild('fileModal') fileModal;
+  progress = 0;
+  modalReference: NgbModalRef;
+  media: Media[];
+  images: Media[];
 
   files = [
     {
@@ -56,9 +42,58 @@ export class MediaComponent implements OnInit {
       name: 'Nombre archivo'
     },
   ];
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private modalService: NgbModal,
+    private mediaService: MediaService
+  ) {
+    this.file = null;
   }
 
+  ngOnInit(): void {
+    this.mediaService.getMediaFiles().subscribe((res: APIResponse) => {
+      this.media = res.data;
+      this.images = res.data;
+    });
+  }
+
+  saveFile(event: any): void {
+    if (event.target.files) {
+      this.file = event.target.files[0];
+    }
+  }
+
+  uploadFile(): void {
+    if (this.file && this.fileName) {
+      this.progress = 0;
+      this.mediaService.uploadMedia(this.file, this.fileName).subscribe((event) => {
+        if (event.type === HttpEventType.UploadProgress) {
+          this.progress = Math.round(100 * event.loaded / event.total);
+        } else if (event instanceof HttpResponse) {
+          Swal.fire('Archivo subido', 'El archivo se subió con exito', 'success');
+          this.mediaService.getMediaFiles().subscribe((res: APIResponse) => {
+            this.images = res.data;
+          });
+          this.modalReference.close();
+        }
+      }, (error) => {
+        console.log(error);
+        Swal.fire('Subida fallida', 'No se pudo subir el archivo', 'error');
+        this.progress = 0;
+        this.file = null;
+        this.fileName = '';
+      });
+    } else {
+      Swal.fire('Datos invalidos', 'Debe agregar un archivo y un nombre para subirlo', 'error');
+    }
+    this.file = null;
+  }
+
+  showFileModal(): void {
+    this.modalReference = this.modalService.open(this.fileModal, { size: 'lg' });
+  }
+
+  closeModal(): void {
+    this.file = null;
+    this.modalReference.close();
+  }
 }
---
+++ src/app/core/services/company/media.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MediaService {
+
+  private static route = `${environment.apiURL}/media`;
+  constructor(private http: HttpClient) { }
+
+  getMediaFiles(): Observable<any> {
+    return this.http.get(MediaService.route);
+  }
+
+  getMediaFile(id: string): Observable<any> {
+    return this.http.get(`${MediaService.route}/${id}`);
+  }
+
+  uploadMedia(file: File, name: string): Observable<any> {
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('media', file);
+
+    const req = new HttpRequest('POST',
+      MediaService.route, formData,
+      { reportProgress: true, responseType: 'json' });
+
+    return this.http.request(req);
+  }
+}
